2a8c2f36efb7da7e165966ded5e8f25fa5560cd6
Anamitra <anamitraupadhyay04@gmail.com>
2026-01-20 13:24:04 +0530

    bad on my part as it seems i mixed up the whole pipeline of udp focuseddiscovery to the pipeline of tcp focused authentication and messed up the whole flow

---
+++ ONVIF-SIM/fakecamera/destroyed_dis_server.c
@@ -0,0 +1,475 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <stdatomic.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MAX_USERS 650
+#define DISCOVERY_PORT 3702
+#define MULTICAST_ADDR "239.255.255.250"
+// #define CAMERA_NAME         "MyFakeCamera"
+#define CAMERA_HTTP_PORT 8080
+#define BUFFER_SIZE 65536
+
+// copied probe match template
+const char *PROBE_MATCH_TEMPLATE =
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+    "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" "
+    "xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" "
+    "xmlns:d=\"http://schemas.xmlsoap.org/ws/2005/04/discovery\" "
+    "xmlns:dn=\"http://www.onvif.org/ver10/network/wsdl\">"
+    "<s:Header>"
+    "<a:Action "
+    "s:mustUnderstand=\"1\">http://schemas.xmlsoap.org/ws/2005/04/discovery/"
+    "ProbeMatches</a:Action>"
+    "<a:MessageID>%s</a:MessageID>"
+    "<a:RelatesTo>%s</a:RelatesTo>"
+    "<a:To>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</"
+    "a:To>"
+    "</s:Header>"
+    "<s:Body>"
+    "<d:ProbeMatches>"
+    "<d:ProbeMatch>"
+    "<a:EndpointReference>"
+    "<a:Address>urn:uuid:fakecam-0001</a:Address>"
+    "</a:EndpointReference>"
+    "<d:Types>dn:NetworkVideoTransmitter</d:Types>"
+    "<d:Scopes>onvif://www.onvif.org/name/%s "
+    "onvif://www.onvif.org/hardware/FakeCam "
+    "onvif://www.onvif.org/type/video_encoder</d:Scopes>"
+    "<d:XAddrs>http://%s:%d/onvif/device_service</d:XAddrs>"
+    "<d:MetadataVersion>1</d:MetadataVersion>"
+    "</d:ProbeMatch>"
+    "</d:ProbeMatches>"
+    "</s:Body>"
+    "</s:Envelope>";
+
+// from close observation there are 5 fields to be extracted
+// 1. is it probe or discovery or not
+// 2. uuid MEssageID from relatesTo
+// 3. localip
+
+// checking its probe or discovery
+bool isprobe(const char *msg);
+bool isprobe(const char *msg) {
+  // does it contain Probe and discovery
+  if (strstr(msg, "Probe") &&
+      strstr(msg, "http://schemas.xmlsoap.org/ws/2005/04/discovery")) {
+    return true;
+  }
+  return false;
+}
+
+// copy pasted :(
+void getmessageid(const char *msg, char *out, size_t outsize);
+void getmessageid(const char *msg, char *out, size_t out_size) {
+  // Look for <wsa:MessageID> first (most common)
+  const char *start = strstr(msg, "<wsa:MessageID");
+  if (!start) {
+    // Try without namespace prefix
+    start = strstr(msg, "<MessageID");
+  }
+  if (!start) {
+    out[0] = '\0';
+    return;
+  }
+
+  // Find the > after opening tag
+  start = strchr(start, '>');
+  if (!start) {
+    out[0] = '\0';
+    return;
+  }
+  start++; // Skip >
+
+  // Find closing tag
+  const char *end = strstr(start, "</");
+  if (!end) {
+    out[0] = '\0';
+    return;
+  }
+
+  size_t len = (size_t)(end - start);
+  if (len >= out_size)
+    len = out_size - 1;
+
+  memcpy(out, start, len);
+  out[len] = '\0';
+
+  // Trim whitespace
+  while (len > 0 && (out[len - 1] == ' ' || out[len - 1] == '\n' ||
+                     out[len - 1] == '\r')) {
+    out[--len] = '\0';
+  }
+}
+
+void getlocalip(char *buf, size_t size) {
+  int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sockfd < 0) {
+    perror("socket");
+    return;
+  }
+
+  struct sockaddr_in sockaddr;
+  memset(&sockaddr, 0, sizeof(sockaddr));
+  sockaddr.sin_port = htons(9000);
+  sockaddr.sin_family = AF_INET;
+  inet_pton(AF_INET, "8.8.8.8", &sockaddr.sin_addr);
+
+  if (connect(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) < 0) {
+    close(sockfd);
+    strncpy(buf, "127.0.0.1", size);
+    return;
+  }
+
+  struct sockaddr_in name;
+  socklen_t namelen = sizeof(name);
+  getsockname(sockfd, (struct sockaddr *)&name, &namelen);
+
+  inet_ntop(AF_INET, &name.sin_addr, buf, size);
+  close(sockfd);
+}
+
+void generate_uuid(char *buf, size_t size) {
+  memset(buf, 0, size);
+  // for 1st part
+  uint8_t first[16] = {0};
+  int fd = open("/dev/urandom", O_RDONLY);
+  if (fd >= 0) {
+    ssize_t readbuf = read(fd, first, sizeof(first) /*16*/);
+    close(fd);
+  } else {
+    perror("open");
+  }
+  // for the 2nd part
+  // 2 parts 1st 6 and then 8
+  // Set standard UUID bits (Version 4)
+  first[6] = (first[6] & 0x0f) | 0x40; // Version 4
+  first[8] = (first[8] & 0x3f) | 0x80; // Variant 1
+
+  // 4. Format string directly into the output buffer
+  // Structure is 8-4-4-4-12 hex digits
+  snprintf(buf, size,
+           "urn:uuid:%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%"
+           "02x%02x%02x",
+           first[0], first[1], first[2], first[3], first[4], first[5], first[6],
+           first[7], first[8], first[9], first[10], first[11], first[12],
+           first[13], first[14], first[15]);
+}
+
+int build_response(const char *message_id, const char *relates_to_id,
+                   const char *local_ip, char *buf, size_t size,
+                   char *device_name);
+/* Build response copypasted not anymore*/
+int build_response(const char *message_id, const char *relates_to_id,
+                   const char *local_ip, char *buf, size_t size,
+                   char *device_name) {
+
+  int len =
+      snprintf(buf, size, PROBE_MATCH_TEMPLATE,
+               message_id,      // 1. <a:MessageID> (UUID)
+               relates_to_id,   // 2. <a:RelatesTo> (The ID from the request)
+               device_name,     // 3. Device Name
+               local_ip,        // 4. IP Address
+               CAMERA_HTTP_PORT // 5. Port
+      );
+  return len;
+}
+
+void getdevicename(char *device_name, uint8_t buffersize) {
+  memset(device_name, 0, buffersize);
+
+  if (gethostname(device_name, /*sizeof(*/ buffersize /*device_name*/) != 0) {
+    perror("gethostname");
+  }
+}
+
+const char *AUTH_TEMPLATE =
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+    "<s:Envelope "
+    "xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" "
+    "xmlns:a=\"http://schemas.xmlsoap.org/ws/2004/08/addressing\" "
+    "xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/"
+    "oasis-200401-wss-wssecurity-secext-1.0.xsd\" "
+    "xmlns:wsu=\"http://docs.oasis-open.org/wss/2004/01/"
+    "oasis-200401-wss-wssecurity-utility-1.0.xsd\" "
+    "xmlns:tds=\"http://www.onvif.org/ver10/device/wsdl\">"
+
+    "<s:Header>"
+    "<wsse:Security s:mustUnderstand=\"1\">"
+    "<wsse:UsernameToken>"
+    "<wsse:Username>%s</wsse:Username>"
+    "<wsse:Password "
+    "Type=\"http://docs.oasis-open.org/wss/2004/01/"
+    "oasis-200401-wss-username-token-profile-1.0#PasswordDigest\">"
+    "%s</wsse:Password>"
+    "<wsse:Nonce "
+    "EncodingType=\"http://docs.oasis-open.org/wss/2004/01/"
+    "oasis-200401-wss-soap-message-security-1.0#Base64Binary\">"
+    "%s</wsse:Nonce>"
+    "<wsu:Created>%s</wsu:Created>"
+    "</wsse:UsernameToken>"
+    "</wsse:Security>"
+    "</s:Header>"
+
+    "<s:Body>"
+    "<tds:GetDeviceInformation/>"
+    "</s:Body>"
+    "</s:Envelope>";
+
+void extract_username(const char *msg, char *out, size_t out_size);
+void extract_username(const char *msg, char *out, size_t out_size) {
+  const char *start = strstr(msg, "<wsse:Username>");
+  if (!start) {
+    out[0] = 0;
+    return;
+  }
+
+  start += strlen("<wsse:Username>");
+  const char *end = strstr(start, "</wsse:Username>");
+  if (!end) {
+    out[0] = 0;
+    return;
+  }
+
+  size_t len = end - start;
+  if (len >= out_size)
+    len = out_size - 1;
+
+  memcpy(out, start, len);
+  out[len] = '\0';
+}
+
+void extract_passwd(const char *msg, char *out, size_t out_size);
+
+void extract_passwd(const char *msg, char *out, size_t out_size) {
+  const char *start = strstr(msg, "<wsse:Password");
+  if (!start) {
+    out[0] = 0;
+    return;
+  }
+
+  start = strchr(start, '>');
+  if (!start) {
+    out[0] = 0;
+    return;
+  }
+  start++;
+
+  const char *end = strstr(start, "</wsse:Password>");
+  if (!end) {
+    out[0] = 0;
+    return;
+  }
+
+  size_t len = end - start;
+  if (len >= out_size)
+    len = out_size - 1;
+
+  memcpy(out, start, len);
+  out[len] = '\0';
+}
+
+/*void parse_body_auth(const char *msg, char *out, size_t out_size){
+  // memsets required as usual
+  // problem with this approach is that its overwriting the same buf
+  // gonna adopt nvidia like pointers approach that is output in params
+  extract_username(msg, out, out_size);
+  extract_passwd(msg, out, out_size);
+}*/
+
+void parse_body_auth(const char *msg, char *username, size_t ulen,
+                     char *password, size_t plen) {
+  extract_username(msg, username, ulen);
+  extract_passwd(msg, password, plen);
+}
+
+typedef struct {
+  char username[64];
+  char password[64];
+} Credential;
+
+Credential credentials[MAX_USERS];
+int credential_count = 0;
+
+void load_csv() {
+  FILE *fp = fopen("Credentials.csv", "rb");
+  if (!fp) {
+    perror("fopen");
+    return;
+  }
+  char line[256];
+  credential_count = 0;
+
+  // improvement and this block is done using llm will look into this matter
+  while (fgets(line, sizeof(line), fp) && credential_count < MAX_USERS) {
+    line[strcspn(line, "\n")] = 0;
+
+    char *username = strtok(line, ",");
+    char *password = strtok(NULL, ",");
+
+    if (username && password) {
+      strncpy(credentials[credential_count].username, username, 63);
+      strncpy(credentials[credential_count].password, password, 63);
+      credentials[credential_count].username[63] = '\0';
+      credentials[credential_count].password[63] = '\0';
+      credential_count++;
+    }
+  }
+  fclose(fp);
+}
+
+int build_auth(char *buffer, char *username, char *password_digest,
+               char *nonce_base64, char *created_timestamp) {
+  //
+  int n =
+      sprintf(buffer, AUTH_TEMPLATE,
+              username,         // %s
+              password_digest,  // %s (Base64(SHA1(Nonce + Created + Password)))
+              nonce_base64,     // %s
+              created_timestamp // %s (ISO 8601 UTC)
+      );
+  return n;
+}
+
+bool is_auth(const char *buf) {
+  // auth requirements holder
+  // that will be memsets later
+  // in scope of the functions
+  if (strstr(buf, "<wsse:UsernameToken>")) {
+    return true;
+  }
+  return false;
+}
+
+// Disclaimer printf stmts are added by llm
+int main(void) {
+
+  printf("=== WS-Discovery Server ===\n");
+
+  srand((unsigned)time(NULL));
+
+  // Geting local IP
+  char local_ip[64];
+  getlocalip(local_ip, sizeof(local_ip));
+  printf("Local IP: %s\n", local_ip);
+
+  // Getting device name
+  char device_name[64];
+  getdevicename(device_name, 64);
+  printf("device %s", device_name);
+
+  // always on udp server
+  // setupped with port
+  int recieversocketudp = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (recieversocketudp < 0) {
+    perror("socket");
+    return -1;
+  }
+  printf("socket created\n");
+  // explicitly mentioned
+  // about address reuse in header
+  int opt = 1;
+  if (setsockopt(recieversocketudp, SOL_SOCKET, SO_REUSEADDR, &opt,
+                 sizeof(opt)) < 0) {
+    perror("setsockopt failed");
+    // not fatal no need for return
+  }
+
+  // bind to address server side
+  struct sockaddr_in recvside;
+
+  memset(&recvside, 0, sizeof(recvside));
+  recvside.sin_family = AF_INET;
+  recvside.sin_port = htons(DISCOVERY_PORT);
+  recvside.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(recieversocketudp, (struct sockaddr *)&recvside, sizeof(recvside)) <
+      0) {
+    perror("bind");
+    return -2;
+  }
+
+  printf("Bound to port %d\n", DISCOVERY_PORT);
+
+  /* Join multicast group - THIS IS THE KEY PART */
+  struct ip_mreq mreq;
+  mreq.imr_multiaddr.s_addr = inet_addr(MULTICAST_ADDR);
+  mreq.imr_interface.s_addr = INADDR_ANY;
+
+  if (setsockopt(recieversocketudp, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq,
+                 sizeof(mreq)) < 0) {
+    perror("multicast join");
+    close(recieversocketudp);
+    return -3;
+  }
+  printf("Joined multicast %s\n", MULTICAST_ADDR);
+
+  printf("\nListening...  (Ctrl+C to stop)\n\n");
+
+  // setting up buffers
+  char recv_buf[BUFFER_SIZE];
+  char send_buf[BUFFER_SIZE];
+
+  // GET message id that is for relates to id
+  char relates_to_id[256];
+
+  char message_id[46]; // urn:uuid(9)+36chars(uuid)+1\0
+
+  // to represent client side and gonna iterate over
+  struct sockaddr_in client_addr;
+  socklen_t client_len;
+  int probe_count = 0;
+
+  while (1) {
+    client_len = sizeof(client_addr);
+    memset(recv_buf, 0, sizeof(recv_buf));
+
+    ssize_t n = recvfrom(recieversocketudp, recv_buf, sizeof(recv_buf) - 1, 0,
+                         (struct sockaddr *)&client_addr, &client_len);
+
+    if (n <= 0)
+      continue;
+    recv_buf[n] = '\0';
+
+    getmessageid(recv_buf, relates_to_id, sizeof(relates_to_id));
+
+    // these 2 up and down function calls should
+    // be inside here for each m=usnique message parse
+
+    generate_uuid(message_id, 46);
+
+    // Check if it's a probe with error handling
+    // will enhance the error handling later
+    if (!isprobe(recv_buf)) {
+      continue;
+    }
+
+    probe_count++;
+
+    /* Get client IP for printing */
+    char client_ip[64];
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
+    printf("[Probe #%d] from %s\n", probe_count, client_ip);
+
+    // build response and send back
+    int send_len = build_response(message_id, relates_to_id, local_ip, send_buf,
+                                  sizeof(send_buf), device_name);
+
+    // Send back
+    ssize_t sent = sendto(recieversocketudp, send_buf, (size_t)send_len, 0,
+                          (struct sockaddr *)&client_addr, client_len);
+
+    if (sent > 0) {
+      printf("         Sent ProbeMatch (%zd bytes)\n", sent);
+    }
+  }
+  return 0;
+}
