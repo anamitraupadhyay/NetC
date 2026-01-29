ad8be5ae26d788a59f7a53ce0b3f8ac6ecd95cda
anamitraupadhyay <anamitraupadhyay@gmail.com>
2026-01-29 04:41:12 +0000

    just a scratch impl of the csvparser though its failing will sort it out

--- ONVIF-SIM/fakecamera/auth_server.h
+++ ONVIF-SIM/fakecamera/auth_server.h
@@ -13,11 +13,24 @@
 #include "dis_utils.h"
 
 // just blind trust for now
+// now some necessary altercations
+// integration of csvparser and also checking if that user exist or not
 int has_any_authentication(const char *request) {
     // Check for HTTP Standard Auth
+    // main if stmt for now as http digest is the first target
     if (strstr(request, "Authorization: Digest") != NULL || 
         strstr(request, "Authorization: Basic") != NULL) {
-        return 1;
+            //utilities i have bool csvparser but do i have user exist?
+            // no i have to implement it 
+            // but i do have extract user and password from req body
+            char user[64];
+            char pass[64];
+            //char passFromCsv[64]; // better impl this in separate function
+            // change of plans already implemented the strcmp 
+            extract_passwd(request , pass, sizeof(pass));
+            extract_username(request, user, sizeof(user));
+            if(!csvparser(user, pass)) return 1;
+            else return 0;
     }
     // Check for ONVIF WS-Security (XML Body)
     if (strstr(request, "wsse:Security") != NULL || 
