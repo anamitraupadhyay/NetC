a3e7b36e6fc7daa750c5ff61e63ec4ee5b38d3f0
anamitraupadhyay <anamitraupadhyay@gmail.com>
2026-02-03 13:32:57 +0000

    inital commit for the both pipeline of set and delete

---
+++ ONVIF-SIM/fakecamera/authhandler/set_delete.h
@@ -0,0 +1,75 @@
+#include <string.h>
+#include <stdio.h>
+// this file has all the update and delete csv line functions
+
+typedef struct {
+    char username[512];
+    char password[512];
+    char userLevel[512];
+} UserCredsupdate;
+
+int extract_tag(const char *sourceCursor, const char *startTag, const char *endTag, char *destinationArray) {
+    const char *start = strstr(sourceCursor, startTag);
+    if (!start) return 0;
+    
+    // Move pointer to the end of the start tag
+    start += strlen(startTag);
+    
+    const char *end = strstr(start, endTag);
+    if (!end) return 0;
+    
+    // Calculate length of the value
+    long length = end - start;
+    if (length >= 512) length = 512 - 1; // Safety cap
+    
+    strncpy(destinationArray, start, length);
+    destinationArray[length] = '\0'; // Ensure null-termination
+    
+    return 1;
+}
+
+static int numofuserssentupdate = 0;
+// need to find better logic later for num of users sent
+// the design needs to be clever as to parse efficiently 
+static UserCredsupdate usersadd[20] = {0};
+
+void parseSentUsers(const char *request){
+    //const char *start = strstr(request,"<tds:CreateUsers>");
+    //const char *end = strstr(request, "</tds:CreateUsers>");
+    const char *movingCursor = request;// points to baseaddr
+    
+    // as per llm "request" is passed in while loop
+    // due to which its starting from start
+    // do movingCursor as its getting updated each loop 
+    while((movingCursor = strstr(/*request*/movingCursor, "<tds:User>")) != NULL){
+        if(numofuserssentupdate >= 20){
+            printf("for now due to poor design choice the hardcoded buffer size %d overflows\n", numofuserssentupdate); break;
+        }
+        // experimenting with design choices here by using
+        // temporary pointers so we only search inside the current User block
+        // end of this specific user block to limit search scope
+        const char *userEnd = strstr(movingCursor, "</tds:User>");
+        if (!userEnd) break; 
+
+        // Extract Username
+        extract_tag(movingCursor, "<tt:Username>", "</tt:Username>", usersadd[numofuserssentupdate].username);
+        
+        // Extract Password
+        extract_tag(movingCursor, "<tt:Password>", "</tt:Password>", usersadd[numofuserssentupdate].password);
+        
+        // Extract UserLevel
+        extract_tag(movingCursor, "<tt:UserLevel>", "</tt:UserLevel>", usersadd[numofuserssentupdate].userLevel);
+        numofuserssentupdate++; // last marked point
+        // below is pointer arithmetic to move the pointer to later
+        movingCursor = userEnd + strlen("</tds:User>");// <-- ADDED
+    }
+}
+
+void delete_csv_line(){
+    //
+}
+
+void update_csv_line(const char *request){
+    //
+    parseSentUsers(request);
+}
--- ONVIF-SIM/fakecamera/authhandler/setuser.h
+++
@@ -1,5 +0,0 @@
-typedef struct{
-    char username[64];
-    char password[64];
-    // char userlevel[16];
-}setuserstruct;
